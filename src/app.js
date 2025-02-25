903ffa43b8941148a9e3630c73613b3bf97e0e19
Aman Sharma <testfeb0202@gmail.com>
2025-02-25 17:03:33 +0530

    explore routings

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("It will handle requests with /hello url path");
+// route parameters
+app.get('/user/:id', (req, res) => {
+    res.send(`User id is ${req.params.id}`);
 });
 
-app.use((req, res) => {
-    res.send("It will handle all the requests");
+// regular expression in dynamic routes
+app.get('/user/:id(\\d+)', (req, res) => {
+    res.send(`User id is ${req.params.id}`);
+});
+
+// multiple route parameters
+app.get('/user/:id/:name', (req, res) => {
+    res.send(`User id is ${req.params.id} and name is ${req.params.name}`);
+});
+
+// optional parameters
+app.get('/user/:id/:name?', (req, res) => {
+    const { id, name } = req.params;
+    if(name) {
+        res.send(`User id is ${id} and name is ${name}`);
+    } else {
+        res.send(`User id is ${id}`);
+    }
+});
+
+// query parameters
+app.get('/users', (req, res) => {
+    const { page, limit } = req.query;
+    res.send(`Page is ${page} and limit is ${limit}`);
 });
 
 app.listen(3000, () => {
---
+++ src/notes.md
@@ -0,0 +1,6 @@
+The default type of request parameters is String.
+
+$ git push --set-upstream origin main
+🔹 What This Does:
+It pushes your local main branch to the remote main branch.
+It sets up tracking so that future git push and git pull commands work without specifying the branch.
