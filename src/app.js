7117b11d2013de4fbd19f643b2f3a7cd315aae54
Aman Sharma <testfeb0202@gmail.com>
2025-02-28 02:03:39 +0530

    middlewares and error handlers

--- src/app.js
+++ src/app.js
@@ -32,6 +32,53 @@ app.get('/users', (req, res) => {
     res.send(`Page is ${page} and limit is ${limit}`);
 });
 
+app.get('/user', (req, res, next) => {
+    console.log("handling the route user 1");
+    // res.send("Response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 2");
+    res.send("Response 2");
+    next();
+  },
+  (req, res) => {
+    console.log("handling the route user 3");
+    res.send("Response 3");
+  }
+);
+
+// Output:- 
+
+// cmd:-
+//     Server is running on port: 3000
+//     handling the route user 1
+//     handling the route user 2
+//     handling the route user 3
+//     Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+
+// postman:- Response 2
+
+app.use('/', 
+    () => {
+        console.log("/error");
+        next(new Error('Mock Error'));
+    },
+    (err, req, res, next) => {
+        console.log('/user error');
+        next();
+    }
+);
+
+app.get('/user', (req, res) => {
+    console.log('/user');
+    res.send('hello world from /user !');
+});
+// output:-
+//   /error
+//   /user error
+//   /user
+
 app.listen(3000, () => {
     console.log("Server is running on port: 3000");
 });
--- src/notes.md
+++ src/notes.md
@@ -4,3 +4,9 @@ $ git push --set-upstream origin main
 🔹 What This Does:
 It pushes your local main branch to the remote main branch.
 It sets up tracking so that future git push and git pull commands work without specifying the branch.
+
+res.send() doesn't stop function execution by itself.
+If you don’t return after res.send(), code after it will still execute, including next().
+Calling next() after res.send() is a problem if another middleware tries to modify the response.
+
+The middleware with a err parameter is only triggered when next(err) is explicitly called by previous route or middleware.
